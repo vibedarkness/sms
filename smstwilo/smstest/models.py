8893838cf630ef35d37ba8db63544b9cccbe9c33
omzo <omzo@example.com>
2023-03-12 09:10:35 +0100

    twilio

--- smstwilo/smstest/models.py
+++ smstwilo/smstest/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import os
+from twilio.rest import Client
 
 # Create your models here.
 
@@ -10,4 +12,18 @@ class Score(models.Model):
     def __str__(self):
         return str(self.resultat)
 
+        
+    def save(self, *args, **kwargs):
+        if self.resultat < 800:
+            account_sid = "AC9f2ab4cd8bfc02faf81504153c0240af"
+            auth_token = "c03f23145833dcf4d66797b82de385e7"
+            client = Client(account_sid, auth_token)
+            message = client.messages.create(
+            body="bonjour oumar votre resultat est {self.resultat}",
+            from_="+15747013851",
+            to="+221786351787"
+            )
+            print(message.sid)
+            
+        return super().save(*args, **kwargs) # Call the real save() method
 
